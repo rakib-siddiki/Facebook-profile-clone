1ca5b669abcdf2ad02d5723763c65f1cdd035436
rakib-siddiki <rmrakib901@gmail.com>
2023-11-29 14:18:18 +0600

    reg page

--- src/main.jsx
+++ src/main.jsx
@@ -5,16 +5,11 @@ import "./index.css";
 import app from "../firebaseConfig";
 import SingUp from "./pages/signUp/SignUp";
 import SignIn from "./pages/signIn/SignIn";
-<<<<<<< HEAD
 import ForgotPassword from "./pages/forgetPassword/ForgetPassword";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { store } from "./store";
 import { Provider } from "react-redux";
 
-=======
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import ForgotPassword from "./pages/forgetPassword/ForgetPassword";
->>>>>>> origin/master
 const router = createBrowserRouter([
   {
     path: "/sign-up",
@@ -29,15 +24,9 @@ const router = createBrowserRouter([
   }
 ]);
 ReactDOM.createRoot(document.getElementById("root")).render(
-<<<<<<< HEAD
   <Provider store={store}>
     <React.StrictMode>
       <RouterProvider router={router} />
     </React.StrictMode>
   </Provider>
-=======
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
->>>>>>> origin/master
 );
